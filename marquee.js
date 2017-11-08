1256b6badd4539db826471159f4e2b97fcc2c87c
ReedSun <superreedsun@gmail.com>
2017-11-08 22:38:58 +0800

    add marquee animation

--- marquee.js
+++ marquee.js
@@ -2,4 +2,16 @@ const marquee = (config) => {
   const el = config.el
   const word = el.innerText
   console.log(word)
+
+  el.innerHTML = `
+    <div style="margin-right: 0px; float: left;">${word}</div>
+    <style>
+      @-webkit-keyframes marqueeAnimation {
+        100% {
+          margin-left:-960px
+        }
+      }
+    </style>
+  `
+  el.setAttribute('style', `width: 100000px; margin-left: 960px; animation: marqueeAnimation 5.26515s linear 1s infinite;`)
 }
